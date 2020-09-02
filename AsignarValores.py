bb8c35fbd8a66e23ba353abb7d0ec0b9b0ff8e01
Freddy875 <fer875nandosm@gmail.com>
2020-09-01 20:15:52 -0500

    Aprendimos como asignar valores

---
+++ AsignarValores.py
@@ -0,0 +1,80 @@
+iNumero1 = 10
+
+print("Hola Mundo")
+
+print(iNumero1)
+
+print(type(iNumero1))
+
+fNumero2 = 10.56
+
+print(fNumero2)
+
+print(type(fNumero2))
+
+cCadena1 = 'A'
+
+print(cCadena1)
+
+print(type(cCadena1))
+
+sCadena2 = "Freddy"
+
+print(sCadena2)
+
+print(type(sCadena2))
+
+sCadena3 = "Estoy 'estudiando' Python"
+
+print(sCadena3)
+
+print(type(sCadena3))
+
+valorVerdadero = True
+
+print(valorVerdadero)
+
+print(type(valorVerdadero))
+
+valorFalso = False
+
+print(valorFalso)
+
+print(type(valorFalso))
+
+fNumero3 = 10
+
+fNumero4 = 6.7
+
+fSuma = fNumero3 + fNumero4
+
+print(fSuma)
+
+fNumero5 = 10
+
+fNumero6 = 6.7
+
+fResultado1 = (fNumero5 + fNumero6) * 10 / 6
+
+print("El resultado es: " ,fResultado1)
+
+fNumero7 = 5.6
+
+fNumero8 = 6.7
+
+fResultado2 = fNumero7 + fNumero8
+
+print("El resultado de sumar: " ,fNumero7 ," mas " , fNumero8 , " es: " , fResultado2)
+
+valor = 10
+
+print(valor)
+
+print(type(valor))
+
+valor = "Freddy"
+
+print(valor)
+
+print(type(valor))
+
