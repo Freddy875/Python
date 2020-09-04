7ddc1dc25602bdea8627625ea0efa754bb9d20f4
Freddy875 <fer875nandosm@gmail.com>
2020-09-04 12:16:38 -0500

    Aprendimos como poner comentarios

--- AsignarValores.py
+++ AsignarValores.py
@@ -1,63 +1,78 @@
 iNumero1 = 10
-
+#Creamos una varial llamada iNumero1 y le asignamos el valor de 10
+#La variable iNumero1 tiene un i al princpio para indicar que es de tipo int (entero)
 print("Hola Mundo")
-
+#Impirminos nuestro clasico Hola Mundo
 print(iNumero1)
-
+#Imprimimos el valor de iNumero1
 print(type(iNumero1))
-
+#Se utiliza el type para ver que tipo de dato es en este caso es de tipo entero
 fNumero2 = 10.56
-
+#Creamos un nuevo tipo de dato llamado fNumero2
+#La variable fNumero2 tiene un f al principio para indicar que es de tipo float (real)
 print(fNumero2)
-
+#Imprimimos el valor de fNumero2
 print(type(fNumero2))
-
+#Nos muestra que es de tipo float 
 cCadena1 = 'A'
-
+#Ahora creamos una variable llamada cCadena1 y le asignasmo solo un caracter
+#Le pusimos c al principio del nombr de la variable porque solo es un caracter
 print(cCadena1)
-
+#Imprimimos el valor de cCadena1 que es A
 print(type(cCadena1))
-
+#cCadena aunque solo sea un caracter es de tipo String
 sCadena2 = "Freddy"
-
+#Creamos otra variable llamada sCdaena2
+#Le pusimos s al principo del nombre de la variable porque ahora es una cadena 
 print(sCadena2)
-
+#Nos imrpime el valor de cCadena2 que es Freddy
 print(type(sCadena2))
-
+#sCadena2 tambien es de tipo String
 sCadena3 = "Estoy 'estudiando' Python"
-
+#creamos un tercer valor de tipo String 
 print(sCadena3)
-
+#Imprime el valor de sCadena con los vaores entre comillas simples 
 print(type(sCadena3))
-
-valorVerdadero = True
-
-print(valorVerdadero)
-
-print(type(valorVerdadero))
-
-valorFalso = False
-
-print(valorFalso)
-
-print(type(valorFalso))
-
+#sCadena3 tambien es un String
+bValorVerdadero = True
+#Creamos una variable le pusimos una b al principo para indicar que es de tipo booleano 
+print(bValorVerdadero)
+#Imprime el valor de la variable que por defecto sabemos que es True (Verdadero)
+print(type(bValorVerdadero))
+#Imprime que la variable es de tipo bool
+bValorFalso = False
+#Caso contrario al anterior creamos otra variabla de tipo bool pero ahora devuelve un False (Falso)
+print(bValorFalso)
+#Imprime el valor de la variable que por defecto sabemos que es False (Falso)
+print(type(bValorFalso))
+#Tambien es un tipo de dato bool
 fNumero3 = 10
 
 fNumero4 = 6.7
 
 fSuma = fNumero3 + fNumero4
-
+#Creamos tres variables de tipo float 
+#A las dos primeras las inicializamos con un valor 
+#La tercera le asignamos la operacion de sumar el valor de fNumero3 + fNumero4
 print(fSuma)
-
+#Imprimimos el valor de la suma 
 fNumero5 = 10
 
 fNumero6 = 6.7
 
 fResultado1 = (fNumero5 + fNumero6) * 10 / 6
-
+'''
+De igual forma que en la anterior creamos tres nuevas variables
+A la primera es de tipo float aunque tenga un valor de tipo int
+La segunda si tiene un valor de tipo float
+La tercera variabe le asignamos una operación 
+Primero suma los valores de fNumero5 y fNumero6 que es 16.7 
+Luego divide 10 / 6 que es 1.66...
+Y multiplicamos ambos resultados para obtener
+27.833333333333332
+'''
 print("El resultado es: " ,fResultado1)
-
+#Imrpimimos el resultado primero con una cadena para indicar parte de lo que hicimos
 fNumero7 = 5.6
 
 fNumero8 = 6.7
@@ -65,7 +80,13 @@ fNumero8 = 6.7
 fResultado2 = fNumero7 + fNumero8
 
 print("El resultado de sumar: " ,fNumero7 ," mas " , fNumero8 , " es: " , fResultado2)
-
+'''
+De manera similar al ejercicio anterior aqui tambien creamos tres variables
+De tipo float, a las dos primeras les asignamos valores
+A la tercera le asignamos la suma de las dos anteriores
+E impirmimos el resultado 
+En el mensaje indicamos como se realiza la operación y que resultado se obtiene 
+'''
 valor = 10
 
 print(valor)
---
+++ Comentarios.py
@@ -0,0 +1,18 @@
+#Esto es un comentario de una sola linea
+#Python oermite el tipado dinámico
+
+
+'''
+Esto es un comentario de multiples lineas 
+El tipado dinámico es que una misma variable puede
+contener diferente tipos de datos a lo largo del programa 
+'''
+
+valor = 10
+
+print(valor)
+
+valor = "Freddy"
+
+print(valor)
+
