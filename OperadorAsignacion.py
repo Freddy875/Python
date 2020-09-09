d2dbabc635f2887ed7602f74cf7d01f97e9f94ef
Freddy875 <fer875nandosm@gmail.com>
2020-09-08 19:37:54 -0500

    Aprendimos como es la salida de datos

---
+++ OperadorAsignacion.py
@@ -0,0 +1,16 @@
+#Operadores de asignación 
+
+NumeroA = 0
+
+#NumeroA = NumeroA + 5
+
+NumeroA += 5 #Suma en asignación 
+NumeroA -= 2 #Resta en asignación 
+NumeroA *= 3 #Multiplicación en asignación 
+NumeroA /= 3 #División en asignación 
+NumeroA **= 2 #Potencia en asignación 
+NumeroA %= 2 #Modulo en asignación 
+
+print(NumeroA)
+
+
---
+++ SalidaDeDatos.py
@@ -0,0 +1,10 @@
+#Salidas
+
+nombre = "Freddy"
+edad = 23 
+
+print("Hola " ,nombre, " tienes ", edad, "anios ")
+
+print("Hola {} tienes {} anios".format(nombre,edad))
+
+print(f"Hola {nombre} tienes {edad} anios")
