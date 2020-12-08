c17da9e62953689a627399df9c79a55fad945c3e
Freddy875 <fer875nandosm@gmail.com>
2020-12-08 16:32:40 -0600

    Le agregamos un input

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,4 +1,5 @@
-#Este codigo imprime un mensaje en pantalla 
-#Esto es un comentario 
+#Este codigo imprime un mensaje en pantalla
+#Esto es un comentario
 print("Hola Mundo \n Bienvenido al Mundo de Python")
 print("Mi nombre es Freddy875")
+input()
