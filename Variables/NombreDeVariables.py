5959ea2c5fa45fa03ee55a5746aab55ae1d8e550
Freddy875 <fer875nandosm@gmail.com>
2021-01-01 16:42:50 -0600

    Aprendimos sobre las reglas de las variables

---
+++ Variables/NombreDeVariables.py
@@ -0,0 +1,22 @@
+"""
+Reglas para varaibles 
+1. El nombre de una variable debe de comenzar con una letra o con un caracter de subrayado
+2. El nombre deuna variable no puede comenzar con un numero 
+3. El nmnbre de una variable solo puede contener caracteres alfanumericos y guiones bajos
+4. Los nombres de una variable distingen entre mayusculas y minusculas
+"""
+
+myvar = "Freddy"
+my_var = "Freddy"
+_my_var = "Freddy"
+myVar = "Freddy"
+MYVAR = "Freddy"
+myvar2 = "Freddy"
+print("Ejemplos")
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
