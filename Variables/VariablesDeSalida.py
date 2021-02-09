4062c1ae7bfbfdadaefc9c78e2b195bcc95e303b
Freddy875 <fer875nandosm@gmail.com>
2021-02-08 20:09:13 -0600

    La declaración print de Python se usa a menudo para generar varibles

---
+++ Variables/VariablesDeSalida.py
@@ -0,0 +1,36 @@
+'''
+La declaración print de Python se usa a menudo 
+para generar varibles
+Para combinar texto y una vairble se usa 
+el caracter +
+'''
+
+x = "asombroso"
+print("Python es " + x)
+
+'''
+Tambien se puede usar el caracter +
+para agregar una varible a otra
+'''
+
+x = "Python es"
+y = " asombroso"
+print(x + y)
+
+'''
+Para los numeros el caracter +
+funciona como operador matematico
+'''
+x = 10
+y = 5 
+print(x +y)
+
+'''
+Si intenta combinar una cadena 
+y un numero 
+Python le dara un error
+x = 5 
+y = "John"
+print(x + y)
+'''
+
