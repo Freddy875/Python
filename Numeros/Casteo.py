e03653b4c6676f49c07f4dad32c1d2ca93e7ccd0
Freddy875 <fer875nandosm@gmail.com>
2021-02-14 18:16:42 -0600

    Arendimos a realizar casteos

---
+++ Numeros/Casteo.py
@@ -0,0 +1,43 @@
+#Python Csting
+
+'''
+Especificar un tipo variable
+Habara ocasiones en las que tengas que 
+especificar un tipo de dato variables.
+Esto se puede hacer con un casteo
+Python es un lenguaje orientado a objetos 
+y como tal se usan clases para definir tipos de datos 
+incluso los tipos de datos primitivos
+'''
+
+print("Catear enteros")
+
+x = int(1) #x sera 1
+y = int(2.8) #y sera 2
+z = int("3") #z sera un 3
+
+print(x)
+print(y)
+print(z)
+
+print("Castear flotantes")
+
+x = float(1) #x sera 1.0
+y = float(2.8) #y sera 2.8
+z = float("3") #z sera un 3.0
+w = float("4.2") #w sera 4.2
+
+print(x)
+print(y)
+print(z)
+print(w)
+
+print("Castear cadenas")
+
+x = str("s1") #x sera 's1'
+y = str(2) #y sera '2'
+z = str(3.0) #z sera '3.0'
+
+print(x)
+print(y)
+print(z)
