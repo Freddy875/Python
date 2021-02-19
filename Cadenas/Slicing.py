7ee4761ed5ebc917577dec0f21aba6e59a2f00c5
Freddy875 <fer875nandosm@gmail.com>
2021-02-18 22:51:04 -0600

    Aprendimos a deslizarnos sobre cadenas

---
+++ Cadenas/Slicing.py
@@ -0,0 +1,27 @@
+'''
+Devuelve un rango de caracteres
+para usar la sintaxis de slice o deslizar
+Especifica el indice de inicio, y el indice final
+separado por una coma que devuelve la parte de la
+cadena
+'''
+a = "Hola Mundo"
+print(a[2:5])
+
+#Deslizar al inicio
+b = "Hola Mundo"
+print(b[:5])
+
+#Deslizar al final 
+c = "Hola Mundo"
+print(c[2:])
+
+'''
+Deslice negativo
+Usar indices negativos a el inicio 
+el deslice a el final de la cadena
+'''
+d = "Hola Mundo"
+print(b[-5:-2])
+
+
