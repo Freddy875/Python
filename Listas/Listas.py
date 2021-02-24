710f9cf73454b2ca1d789bcc27ebf4e3895b1645
Freddy875 <fer875nandosm@gmail.com>
2021-02-23 20:04:58 -0600

    Aprendimos sobre las listas

---
+++ Listas/Listas.py
@@ -0,0 +1,73 @@
+'''
+Las listas son usadas para alacenar multiples valores en una variale
+'''
+
+estaLista = ["manzana","platanos","cerezas"]
+print(estaLista)
+
+'''
+-Los elemento en las listas son ordenados, cambiables 
+y permite valores duplicados
+
+-Los elementos estan indexados, el primer elemento tiene un 
+indice[0] el segundo elemnto tiene el indice [1],etc
+
+-Cuando decimos que las listas son ordenadas, significa que 
+los elementos tienen un orden definido, y el orden no cambiara
+Si tu agregas un elemento a la lista, el nuevo elementos 
+se pocicionara al final de la lista        
+
+-Cambiable signfiica que podemos cambiar, agregar y remover 
+elementos en la lista despues de que ha sido creada
+'''
+
+estaLista = ["manzana","platanos","cerezas","manzana","cereza"]
+print(estaLista)
+
+'''
+Longitud de la lista 
+Para determinuar cuantos elementos tiene una lista
+usamos la funcion len()
+'''
+estaLista = ["manzana","platanos","cerezas"]
+print(len(estaLista))
+
+'''
+Las listas pueden ser de cualquier tipo de dato
+'''
+
+lista1 = ["manzana","platanos","cerezas"]
+lista2 = [1,5,7,9,3]
+lista3 = [True,False,True]
+
+print(lista1)
+print(lista2)
+print(lista3)
+
+'''
+Una lista puede contene diferentes tipos de datos
+'''
+lista1 = ["abc",34,True,40,"male"]
+print(lista1)
+
+'''
+Tipo Lista
+'''
+lista1 = ["manzana","platanos","cerezas"]
+print(type(lista1))
+
+#Construir una lista
+estaLista = list(("manzana","platanos","cerezas"))
+print(estaLista)
+
+
+
+
+
+
+
+
+
+
+
+
