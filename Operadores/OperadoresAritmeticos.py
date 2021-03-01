569feb01d44f70ff5e246d414c783f8d51789d4b
Freddy875 <fer875nandosm@gmail.com>
2021-02-28 19:10:30 -0600

    Aprendimos sobre las operaciones de aritmetica

---
+++ Operadores/OperadoresAritmeticos.py
@@ -0,0 +1,51 @@
+'''
+Operadores son usados para desempenio de operaciones con valores y variables
+'''
+
+#Operadores artimeticos
+
+x = 5
+y = 3 
+#Suma
+print("Suma")
+print(x + y)
+
+#Resta
+print("Resta")
+print(x - y)
+
+#Multiplicacion
+print("Multipliacion")
+print(x*y)
+
+#Division
+x = 12
+y = 3 
+print("Division")
+print(x/y)
+
+#Modulos
+x = 5
+y = 2 
+print("Modulos")
+print(x%y)
+
+#Exponencial
+x = 2
+y = 5 
+print("Exponenciales")
+print(x ** y)
+
+#Division Floor 
+#Redondea el resultado al numero entero más cercano
+x = 15
+y = 2
+print("Exponenciales")
+print(x // y)
+
+
+
+
+
+
+
