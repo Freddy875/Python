08754221ed23aadb75ceed1e3038f127bb5006de
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-01 15:10:44 -0600

    Create Metodo_String.py
    Ahora trabajamos con metodos de string

---
+++ Metodo_String.py
@@ -0,0 +1,42 @@
+frase = "Especialmente en las comunicaciones electrónicas, la escritura enteramente en mayúsculas equivale a gritar."
+
+# Convertir la frase a mayúsculas
+frase_mayusculas = frase.upper()
+
+# Mostrar el resultado
+print(frase_mayusculas)
+
+---
+
+lista_palabras = ["La", "legibilidad", "cuenta."]
+
+# Unir la lista en un string con espacios
+frase_unida = " ".join(lista_palabras)
+
+# Mostrar el resultado
+print(frase_unida)
+
+---
+
+# Definir el haiku
+haiku = """Tierra mojada,
+mis recuerdos de viaje
+entre las lluvias"""
+
+# Verificar si "agua" NO está en el haiku
+resultado = "agua" not in haiku
+
+# Mostrar el resultado
+print(resultado)
+
+---
+
+# Definir la palabra
+palabra = "electroencefalografista"
+
+# Obtener el número de caracteres
+longitud = len(palabra)
+
+# Mostrar el resultado
+print(longitud)
+
