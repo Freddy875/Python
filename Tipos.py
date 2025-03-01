f088594bea41e9d0e1f6ee4be20f5bba385aaf85
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-01 11:58:25 -0600

    Create Tipos.py
    Un programa para ver los tipos de datos

---
+++ Tipos.py
@@ -0,0 +1,41 @@
+"""
+Declara una variable numérica llamada num_entero que contenga un valor de tipo integer de tu elección.
+
+Imprime el tipo de dato de dicha variable.
+
+"""
+
+num_entero = 42  
+print(type(num_entero))
+
+
+"""
+Declara una variable numérica llamada num_decimal que contenga un valor de tipo float de tu elección.
+
+Imprime el tipo de dato de dicha variable.
+"""
+
+num_decimal = 3.14      
+print(type(num_decimal))
+
+"""
+¿De qué tipo es el resultado de la suma de 7.5 + 2.5? Genera el código para verificarlo.
+
+Para ello, crea dos variables:
+
+num1 = 7.5
+
+num2 = 2.5
+
+A continuación, muestra en pantalla el tipo de dato que resulta de la suma de ambos números.
+"""
+
+num1 = 7.5
+num2 = 2.5
+
+resultado = num1 + num2  # Realizamos la suma
+print(type(resultado))  # Mostramos el tipo de dato del resultado
+
+
+
+
