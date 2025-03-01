6a84f87e844c42e7b29a1ea2b2e2eeda3a1da7f5
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-01 14:54:26 -0600

    Create Slicing.py
    Ahora trabajando con el metodo Slicing

---
+++ Slicing.py
@@ -0,0 +1,27 @@
+frase = "Controlar la complejidad es la esencia de la programación"
+
+# Extraer la primera palabra usando slicing
+primera_palabra = frase[:9]
+
+# Mostrar el resultado
+print(primera_palabra)
+
+---
+
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+
+# Extraer cada tercer carácter desde el noveno hasta el final
+resultado = frase[8::3]
+
+# Mostrar el resultado
+print(resultado)
+
+---
+
+frase = "Es genial trabajar con ordenadores. No discuten, lo recuerdan todo y no se beben tu cerveza"
+
+# Invertir la frase usando slicing
+frase_invertida = frase[::-1]
+
+# Mostrar el resultado
+print(frase_invertida)
