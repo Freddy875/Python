3cd62972c0d7456ed46edafcdd80b09975f70670
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-01 17:43:13 -0600

    Create Tuplas.py
    Trabajmos con tuplas

---
+++ Tuplas.py
@@ -0,0 +1,33 @@
+# Definir la tupla
+mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+
+# Contar cuántas veces aparece el número 2
+cantidad_dos = mi_tupla.count(2)
+
+# Mostrar el resultado
+print(cantidad_dos)
+
+---
+
+# Definir la tupla
+mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2)
+
+# Convertir la tupla en una lista
+mi_lista = list(mi_tupla)
+
+# Mostrar la lista resultante
+print(mi_lista)
+
+---
+
+# Definir la tupla
+mi_tupla = (1, 2, 3, 4)
+
+# Desempaquetar los valores en variables
+a, b, c, d = mi_tupla
+
+# Mostrar los valores de cada variable
+print(a)  # 1
+print(b)  # 2
+print(c)  # 3
+print(d)  # 4
