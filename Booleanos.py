b1d1b28dfe223db33e1cd8fd5322a8e2cd9b0e42
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-01 18:01:34 -0600

    Create Booleanos.py
    Trabajamos con booleanos

---
+++ Booleanos.py
@@ -0,0 +1,21 @@
+# Realizar una comparación y almacenar el resultado en 'prueba'
+prueba = 10 > 5  # Esto dará True
+
+# Mostrar el resultado
+print(prueba)
+
+---
+
+# Realizar la comparación
+resultado = (17834 / 34) > (87 * 56)
+
+# Mostrar el resultado
+print(resultado)
+
+---
+
+# Verificar si la raíz cuadrada de 25 es igual a 5
+resultado = (25 ** 0.5) == 5
+
+# Mostrar el resultado
+print(resultado)
