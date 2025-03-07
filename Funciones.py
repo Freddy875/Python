61c2f9a0fbb7b26d65e9e44ac87e4263ebc8e610
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-07 12:26:19 -0600

    Create Funciones.py
    Trabajamos con las funciones

---
+++ Funciones.py
@@ -0,0 +1,21 @@
+# Definir la función saludar
+def saludar():
+    print("¡Hola mundo!")
+
+---
+
+# Definir la función bienvenida
+def bienvenida(nombre_persona):
+    print(f"¡Bienvenido {nombre_persona}!")
+
+# Crear la variable con un nombre
+nombre_persona = "Carlos"
+
+---
+
+# Definir la función cuadrado
+def cuadrado(un_numero):
+    print(un_numero ** 2)  # Eleva el número al cuadrado y lo imprime
+
+# Crear la variable con un número cualquiera
+un_numero = 7
