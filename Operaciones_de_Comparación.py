495285be8fbdb9fbad0e5e19d4d7ca1dee852490
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-07 10:25:15 -0600

    Create Operaciones_de_Comparación.py
    Trabajamos con operadores de comparación

---
+++ Operaciones_de_Comparación.py
@@ -0,0 +1,36 @@
+# Definir las variables
+num1 = 36
+num2 = 17
+
+# Verificar si num1 es mayor o igual que num2
+mi_bool = num1 >= num2
+
+# Imprimir el resultado
+print(mi_bool)  # Esto imprimirá True
+
+
+---
+
+import math  # Importamos el módulo math para calcular la raíz cuadrada
+
+# Definir las variables
+num1 = math.sqrt(25)  # Raíz cuadrada de 25
+num2 = 5
+
+# Verificar si num1 es igual a num2
+mi_bool = num1 == num2
+
+# Imprimir el resultado
+print(mi_bool)  # Esto imprimirá True
+
+---
+
+# Definir las variables con los resultados de las operaciones
+num1 = 64 * 3  # 64 por 3
+num2 = 24 * 8  # 24 por 8
+
+# Verificar si num1 es diferente a num2
+mi_bool = num1 != num2
+
+# Imprimir el resultado
+print(mi_bool)  # Esto imprimirá True o False según el resultado
