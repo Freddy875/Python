ae8abd888d4958289912e63f3d57d39ddf86794e
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-07 12:23:00 -0600

    Create Metodos.py
    Trabajamos con metodos

---
+++ Metodos.py
@@ -0,0 +1,19 @@
+# Texto original
+texto = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
+
+# Usar lstrip() para eliminar caracteres específicos al inicio
+texto_limpiado = texto.lstrip(",:_#%_")
+
+# Imprimir el resultado
+print(texto_limpiado)
+
+---
+
+# Lista original de frutas
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+
+# Insertar "naranja" en la cuarta posición (índice 3)
+frutas.insert(3, "naranja")
+
+# Imprimir la lista actualizada
+print(frutas)
