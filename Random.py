c84b859ee520a23f2366f88b920759f66f5ee114
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-07 11:42:55 -0600

    Create Random.py
    Traajamos con el metodo Random

---
+++ Random.py
@@ -0,0 +1,30 @@
+import random  # Importamos la librería random
+
+# Generar un número aleatorio entre 1 y 10
+aleatorio = random.randint(1, 10)
+
+# Imprimir el número aleatorio
+print(aleatorio)
+
+---
+
+import random  # Importamos la librería random
+
+# Generar un número decimal aleatorio entre 0 y 1
+aleatorio = random.random()
+
+# Imprimir el número aleatorio
+print(aleatorio)
+
+---
+
+import random  # Importamos la librería random
+
+# Lista de nombres
+nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]
+
+# Seleccionar un nombre aleatorio de la lista
+sorteo = random.choice(nombres)
+
+# Imprimir el nombre seleccionado
+print(sorteo)
