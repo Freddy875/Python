d91d5b62f7b9d987ae65e2510a9ea4331eb972bb
Fernando Sánchez Mejía <60365437+Freddy875@users.noreply.github.com>
2025-03-07 09:09:29 -0600

    Update Día1_Cerveceria.py
    Actualizado para hacer otro tipo de pregunta. Que nombre de cerveza les salio para mi fue una que se llama Jaguar Negro

--- Proyectos/Día1_Cerveceria.py
+++ Proyectos/Día1_Cerveceria.py
@@ -1,11 +1,11 @@
 # Programa para generar un nombre creativo para una cerveza
 
-# Preguntas al usuario
-sabor = input("¿Cuál es tu sabor favorito en una cerveza? (Ejemplo: amargo, dulce, tostado) ")
-animal = input("Menciona un animal que te parezca interesante o divertido: ")
+# Solicitar datos al usuario
+animal = input("¿Cuál es tu animal favorito? ")
+color = input("¿Cuál es tu color favorito? ")
 
 # Generación del nombre de la cerveza
-nombre_cerveza = f"\"{sabor.capitalize()} {animal.capitalize()}\""
+nombre_cerveza = f"\"{animal.capitalize()} {color.capitalize()}\""
 
 # Impresión del resultado en dos líneas
 print("\n¡Felicidades! El nombre de tu cerveza es:")
